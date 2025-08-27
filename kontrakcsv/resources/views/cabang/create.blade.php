937e8b1bc2a46955b753783220cba640541f0482
adriswara <chevellerz454@yahoo.com>
2025-08-27 13:05:44 +0700

    insert for cabang done

--- kontrakcsv/resources/views/cabang/create.blade.php
+++ kontrakcsv/resources/views/cabang/create.blade.php
@@ -1,3 +1,40 @@
-<div>
-    <!-- I have not failed. I've just found 10,000 ways that won't work. - Thomas Edison -->
+@include('component.header')
+<!-- main content -->
+<div class="container mt-5">
+    <h2>Tambah Cabang Baru</h2>
+
+    @if ($errors->any())
+    <div class="alert alert-danger">
+        <strong>Oops!</strong> Ada kesalahan pada input:<br><br>
+        <ul>
+            @foreach ($errors->all() as $error)
+            <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+    @endif
+
+    @if (session('message'))
+    <div class="alert alert-success">
+        {{ session('message') }}
+    </div>
+    @endif
+
+    <form action="{{ url('/cabang/store') }}" method="POST">
+        @csrf
+
+        <div class="mb-3">
+            <label for="kode_cabang" class="form-label">Kode Cabang</label>
+            <input type="text" name="kode_cabang" class="form-control" value="{{ old('kode_cabang') }}" placeholder="Contoh: CB006">
+        </div>
+
+        <div class="mb-3">
+            <label for="nama_cabang" class="form-label">Nama Cabang</label>
+            <input type="text" name="nama_cabang" class="form-control" value="{{ old('nama_cabang') }}" placeholder="Contoh: Cabang Semarang">
+        </div>
+
+        <button type="submit" class="btn btn-primary">Simpan</button>
+    </form>
 </div>
+<!-- end main content -->
+@include('component.footer')
