37c4da00142fe5173b21f577e1d3ff2883ad3f8e
adriswara <chevellerz454@yahoo.com>
2025-08-28 15:05:55 +0700

    fixed routing method and tested for all soft delete

--- kontrakcsv/routes/web.php
+++ kontrakcsv/routes/web.php
@@ -14,14 +14,14 @@ Route::get('/pegawai/create', [Pegawai::class, 'create'])->name('pegawai.create'
 Route::post('/pegawai/store', [Pegawai::class, 'store'])->name('pegawai.store');
 Route::get('/pegawai/edit/{id}', [Pegawai::class, 'edit'])->name('pegawai.edit');
 Route::put('/pegawai/update/{id}', [Pegawai::class, 'update'])->name('pegawai.update');
-Route::post('/pegawai/destroy/{id}', [Pegawai::class, 'destroy'])->name('pegawai.destroy');
+Route::delete('/pegawai/destroy/{id}', [Pegawai::class, 'destroy'])->name('pegawai.destroy');
 
 Route::get('/jabatan', [Jabatan::class, 'index'])->name('jabatan.index');
 Route::get('/jabatan/create', [Jabatan::class, 'create'])->name('jabatan.create');
 Route::post('/jabatan/store', [Jabatan::class, 'store'])->name('jabatan.store');
 Route::get('/jabatan/edit/{id}', [Jabatan::class, 'edit'])->name('jabatan.edit');
 Route::put('/jabatan/update/{id}', [Jabatan::class, 'update'])->name('jabatan.update');
-Route::post('/jabatan/destroy/{id}', [Jabatan::class, 'destroy'])->name('jabatan.destroy');
+Route::delete('/jabatan/destroy/{id}', [Jabatan::class, 'destroy'])->name('jabatan.destroy');
 
 Route::get('/cabang', [Cabang::class, 'index'])->name('cabang.index');
 Route::get('/cabang/create', [Cabang::class, 'create'])->name('cabang.create');
